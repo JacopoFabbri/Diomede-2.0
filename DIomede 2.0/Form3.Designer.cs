c31265d07d937a9186dc2c51db7bc69fd0095118
Jacopo Fabbri <jacopo.fabbri.work@gmail.com>
2019-06-05 17:06:18 +0200

    adfgf

--- DIomede 2.0/Form3.Designer.cs
+++ DIomede 2.0/Form3.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(516, 474);
             this.Name = "Form3";
             this.Text = "Form3";
             this.Load += new System.EventHandler(this.Form3_Load);
