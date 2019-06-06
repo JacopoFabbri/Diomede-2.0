7e65b41422b1400161f6eca751a565af19ac2f41
Jacopo Fabbri <jacopo.fabbri.work@gmail.com>
2019-06-06 10:47:45 +0200

    gdfgadfdfag

--- DIomede 2.0/Form5.cs
+++ DIomede 2.0/Form5.cs
@@ -12,9 +12,18 @@ namespace Diomede2
 {
     public partial class Form5 : Form
     {
-        public Form5()
+        String db;
+        Cliente cliente;
+        public Form5(Cliente c, String dbName)
         {
+            cliente = c;
+            db = dbName;
             InitializeComponent();
+        }
+
+        private void Form5_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
