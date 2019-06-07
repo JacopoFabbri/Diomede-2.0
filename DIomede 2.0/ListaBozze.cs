70e5f1af68574ad0b29747d4e892384bdf19fa5f
Jacopo Fabbri <jacopo.fabbri.work@gmail.com>
2019-06-07 14:54:13 +0200

    fsgadfhga

---
+++ DIomede 2.0/ListaBozze.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Diomede2
+{
+    public partial class ListaBozze : Form
+    {
+        readonly String db;
+        OperazionePraticheEdili op;
+        readonly Dashboard formPrecente;
+        public ListaBozze(String dbName, Dashboard frm)
+        {
+            formPrecente = frm;
+            db = dbName;
+            InitializeComponent();
+        }
+
+        private void ListaBozze_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                op = new OperazionePraticheEdili(db);
+                if (op.CercaBozza() != null)
+                {
+                    dataGridView1.DataSource = op.CercaBozza();
+                    dataGridView1.Columns[0].Visible = false;
+                }
+                else
+                {
+                    dataGridView1.DataSource = op.CercaBozza();
+
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Impossibile accedere a quest'area !!!");
+                Application.Exit();
+            }
+            formPrecente.Hide();
+        }
+        private void DataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            foreach (DataGridViewCell cella in dataGridView1.Rows[e.RowIndex].Cells)
+            {
+                cella.Style.ForeColor = Color.Red;
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
