b754397fb85110cbd9fb0985a304a8d2e0b17f07
Jacopo Fabbri <jacopo.fabbri.work@gmail.com>
2019-06-17 10:20:10 +0200

    dsgdfgh

--- DIomede 2.0/TIpologieMacrolavorazioni.cs
+++ DIomede 2.0/TIpologieMacrolavorazioni.cs
@@ -13,15 +13,55 @@ namespace Diomede2
     public partial class TIpologieMacrolavorazioni : Form
     {
         readonly String db;
+        OperazionePraticheEdili op;
         public TIpologieMacrolavorazioni(String dbName)
         {
             db = dbName;
             InitializeComponent();
         }
 
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            InserimentoTipologiaMacrolavorazione iL = new InserimentoTipologiaMacrolavorazione(db);
+            iL.Show();
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow riga in dataGridView1.Rows)
+            {
+                if (riga.Cells[0].Value != null)
+                {
+                    if (riga.Cells[0].Style.ForeColor == Color.Red)
+                    {
+                        try
+                        {
+                            op.UpdateTipologiaMacroLavorazione((int)riga.Cells["ID"].Value, riga.Cells["NOME"].Value + "", (int)riga.Cells["DESC"].Value + "");
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Errore nell'inserimento di dati controllare l'inserimento", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                        }
+                    }
+                }
+            }
+            dataGridView1.DataSource = op.CercaTipologiaMacroLavorazione();
+            dataGridView1.Columns[0].Visible = false;
+        }
         private void TIpologieMacrolavorazioni_Load(object sender, EventArgs e)
         {
+            try
+            {
+                op = new OperazionePraticheEdili(db);
+                dataGridView1.DataSource = op.CercaTipologiaMacroLavorazione();
+                dataGridView1.Columns[0].Visible = false;
 
+            }
+            catch
+            {
+                MessageBox.Show("Impossibile accedere a quest'area !!!");
+                Application.Exit();
+            }
         }
     }
 }
