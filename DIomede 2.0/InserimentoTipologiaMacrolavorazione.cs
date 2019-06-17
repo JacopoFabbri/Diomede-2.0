80beba7777db9fcd5415202ebd2039fdc48ca99f
Jacopo Fabbri <jacopo.fabbri.work@gmail.com>
2019-06-17 09:36:23 +0200

    tgergsh

---
+++ DIomede 2.0/InserimentoTipologiaMacrolavorazione.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Diomede2
+{
+    public partial class InserimentoTipologiaMacrolavorazione : Form
+    {
+        readonly String db;
+        public InserimentoTipologiaMacrolavorazione(String dbName)
+        {
+            db = dbName;
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OperazionePraticheEdili op = new OperazionePraticheEdili(db);
+                op.InserimentoTipologiaMacrolavorazione(textBox1.Text, textBox2.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Errore durante l'inserimento \nripetere l'operazione");
+            }
+        }
+    }
+}
