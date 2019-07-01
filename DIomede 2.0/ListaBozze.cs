e18cd7099224dff89f51a67d8a35d18185c15f18
Jacopo Fabbri <jacopo.fabbri.work@gmail.com>
2019-07-01 15:15:57 +0200

    2315641

--- DIomede 2.0/ListaBozze.cs
+++ DIomede 2.0/ListaBozze.cs
@@ -67,7 +67,7 @@ namespace Diomede2
                             if ((Boolean)riga.Cells["ACCETTAZIONE"].Value == true)
                             {
                                 op.InserimentoCommessa((int)riga.Cells["CLIENTE"].Value, riga.Cells["NUMEROCOMMESSA"].Value + "", (DateTime)riga.Cells["DATA"].Value, "", (int)riga.Cells["ID"].Value);
-                                ListaMacroLavorazioni frm = new ListaMacroLavorazioni(db,(int) riga.Cells["ID"].Value );
+                                ListaMacroLavorazioni frm = new ListaMacroLavorazioni(db, (int)riga.Cells["ID"].Value);
                                 frm.Show();
 
                             }
@@ -112,6 +112,66 @@ namespace Diomede2
         private void ListaBozze_FormClosing(object sender, FormClosingEventArgs e)
         {
             formPrecente.Show();
+        }
+
+        private void AggiornaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow riga in dataGridView1.Rows)
+            {
+                if (riga.Cells[0].Value != null)
+                {
+                    if (riga.Cells[0].Style.ForeColor == Color.Red)
+                    {
+                        try
+                        {
+                            op.UpdateBozza((int)riga.Cells["ID"].Value, (DateTime)riga.Cells["DATA"].Value, riga.Cells["PACCHETTO"].Value + "", (double)riga.Cells["IMPORTO"].Value, riga.Cells["NUMEROCOMMESSA"].Value + "", (int)riga.Cells["CLIENTE"].Value, (Boolean)riga.Cells["ACCETTAZIONE"].Value);
+                            if ((Boolean)riga.Cells["ACCETTAZIONE"].Value == true)
+                            {
+                                op.InserimentoCommessa((int)riga.Cells["CLIENTE"].Value, riga.Cells["NUMEROCOMMESSA"].Value + "", (DateTime)riga.Cells["DATA"].Value, "", (int)riga.Cells["ID"].Value);
+                                ListaMacroLavorazioni frm = new ListaMacroLavorazioni(db, (int)riga.Cells["ID"].Value);
+                                frm.Show();
+
+                            }
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Errore nell'inserimento di dati controllare l'inserimento", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                        }
+                    }
+                }
+            }
+            dataGridView1.DataSource = op.CercaBozza();
+            dataGridView1.Columns[0].Visible = false;
+
+        }
+
+        private void AggiungiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            InserimentoBozza ib = new InserimentoBozza(db, this);
+            ib.Show();
+        }
+
+        private void EliminaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows != null)
+            {
+                if (MessageBox.Show("Stai per eliminare " + (String)dataGridView1.Rows[dataGridView1.SelectedRows[0].Index].Cells[4].Value + " .Confermi?", "Conferma Eliminazione richiesta:", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning) == DialogResult.Yes)
+                {
+                    try
+                    {
+                        Bozza clienti = op.CercaBozzaId((int)dataGridView1.Rows[dataGridView1.SelectedRows[0].Index].Cells[0].Value);
+                        op.CancellaBozza((int)dataGridView1.Rows[dataGridView1.SelectedRows[0].Index].Cells[0].Value);
+                        MessageBox.Show("Cliente Eliminato", "Conferma", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Impossibile cancellare la riga selezionata", "Errore:", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            dataGridView1.DataSource = op.CercaBozza();
+            dataGridView1.Columns[0].Visible = false;
         }
     }
 }
