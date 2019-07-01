747c00f408ee4e265727554089b7684a7fe31a5e
Jacopo Fabbri <jacopo.fabbri.work@gmail.com>
2019-07-01 15:42:49 +0200

    asfgaf

--- DIomede 2.0/ListaClienti.cs
+++ DIomede 2.0/ListaClienti.cs
@@ -107,5 +107,57 @@ namespace Diomede2
             dataGridView1.DataSource = op.CercaClienti();
             dataGridView1.Columns[0].Visible = false;
         }
+
+        private void AggiornaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow riga in dataGridView1.Rows)
+            {
+                if (riga.Cells[0].Value != null)
+                {
+                    if (riga.Cells[0].Style.ForeColor == Color.Red)
+                    {
+                        try
+                        {
+                            op.UpdateCliente((int)riga.Cells["ID"].Value, riga.Cells["NOME"].Value + "", riga.Cells["INDIRIZZO"].Value + "", riga.Cells["CAP"].Value + "", riga.Cells["CITTA"].Value + "", riga.Cells["PEC"].Value + "", riga.Cells["EMAIL"].Value + "", riga.Cells["Iva"].Value + "", riga.Cells["Tel"].Value + "", riga.Cells["SDI"].Value + "");
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Errore nell'inserimento di dati controllare l'inserimento", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                        }
+                    }
+                }
+            }
+            dataGridView1.DataSource = op.CercaClienti();
+            dataGridView1.Columns[0].Visible = false;
+        }
+
+        private void AggiungiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            InserimentoCliente frm = new InserimentoCliente(db);
+            frm.Show();
+        }
+
+        private void EliminaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows != null)
+            {
+                if (MessageBox.Show("Stai per eliminare " + (String)dataGridView1.Rows[dataGridView1.SelectedRows[0].Index].Cells[1].Value + " .Confermi?", "Conferma Eliminazione richiesta:", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning) == DialogResult.Yes)
+                {
+                    try
+                    {
+                        Cliente clienti = op.CercaClientiId((int)dataGridView1.Rows[dataGridView1.SelectedRows[0].Index].Cells[0].Value);
+                        op.CancellaCliente((int)dataGridView1.Rows[dataGridView1.SelectedRows[0].Index].Cells[0].Value);
+                        MessageBox.Show("Cliente Eliminato", "Conferma", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Impossibile cancellare la riga selezionata", "Errore:", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            dataGridView1.DataSource = op.CercaClienti();
+            dataGridView1.Columns[0].Visible = false;
+        }
     }
 }
