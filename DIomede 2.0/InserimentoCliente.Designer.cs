f1870399c7783e1fa2bf77e68bba9c806df4b3d5
Jacopo Fabbri <jacopo.fabbri.work@gmail.com>
2019-07-04 13:03:56 +0200

    gafgsadf

--- DIomede 2.0/InserimentoCliente.Designer.cs
+++ DIomede 2.0/InserimentoCliente.Designer.cs
@@ -292,7 +292,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.Location = new System.Drawing.Point(220, 211);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(260, 60);
+            this.button1.Size = new System.Drawing.Size(264, 60);
             this.button1.TabIndex = 8;
             this.button1.Text = "Inserisci";
             this.button1.UseVisualStyleBackColor = true;
--- DIomede 2.0/Recap.cs
+++ DIomede 2.0/Recap.cs
@@ -12,9 +12,31 @@ namespace Diomede2
 {
     public partial class Recap : Form
     {
-        public Recap()
+        String db;
+        int idCommessa;
+        OperazionePraticheEdili op;
+        List<Cliente> lista;
+        Commessa commessa;
+        public Recap(String dbName, int id)
         {
+            db = dbName;
+            idCommessa = id;
             InitializeComponent();
+        }
+
+        private void Recap_Load(object sender, EventArgs e)
+        {
+            op = new OperazionePraticheEdili(db);
+            commessa = op.CercaCommessa(idCommessa);
+            textBox1.Text = "" + commessa.Id;
+            textBox2.Text = op.CercaClientiId(commessa.Ditta).Nome;
+            List<MacroLavorazione> listaMacrolavorazione = op.FiltraMacroLavorazione("COMMESSA","" + commessa.Id);
+            String s = "";
+            foreach(MacroLavorazione m in listaMacrolavorazione)
+            {
+                s += m.Nome + "\n";
+            }
+            textBox3.Text = s;
         }
     }
 }
