653de0951c11b5cd50886334a943b577dcb2707f
Andrea Passarella <duddyp@hotmail.com>
2019-07-04 17:40:17 +0200

    hg

--- DIomede 2.0/ListaLavorazioni.Designer.cs
+++ DIomede 2.0/ListaLavorazioni.Designer.cs
@@ -70,7 +70,6 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(891, 441);
             this.dataGridView1.TabIndex = 2;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellContentClick);
             this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellValueChanged);
             // 
             // menuStrip1
