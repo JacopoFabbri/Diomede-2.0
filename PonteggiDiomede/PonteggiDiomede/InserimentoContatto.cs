97d798dd7cb7d85988fb09ed5bc9418c258eb997
Jacopo Fabbri <jacopo.fabbri.work@gmail.com>
2019-07-08 14:42:04 +0200

    fsadgasd

---
+++ PonteggiDiomede/PonteggiDiomede/InserimentoContatto.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Diomede2
+{
+    public partial class InserimentoContatto : Form
+    {
+        private readonly Cliente cliente;
+        private readonly string db;
+        private List<Ruolo> lista;
+
+        public InserimentoContatto(Cliente c, string dbName)
+        {
+            cliente = c;
+            db = dbName;
+            InitializeComponent();
+        }
+
+        public void Form5_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            textBox8.Text = "" + cliente.Id;
+            var op = new OperazionePraticheEdili(db);
+            lista = op.CercaRuolo();
+            if (lista != null)
+                foreach (var r in lista)
+                    comboBox1.Items.Add(r.Nome);
+            else
+                MessageBox.Show("Inserire almeno un ruolo!");
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var op = new OperazionePraticheEdili(db);
+                if (!comboBox1.Text.Equals("Ruolo"))
+                {
+                    op.InserimentoContatto(textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text, textBox5.Text,
+                        textBox6.Text, textBox7.Text, textBox8.Text, textBox9.Text, textBox10.Text,
+                        "" + lista[comboBox1.SelectedIndex].Id);
+                    MessageBox.Show("Contatto Inserito", "Inserito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Close();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Errore durante l'inserimento \nripetere l'operazione");
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void InserisciRuoloToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var frm = new Ruoli(db, this);
+            frm.Show();
+        }
+    }
+}
---
+++ PonteggiDiomede/PonteggiDiomede/Ruoli.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Diomede2
+{
+    public partial class Ruoli : Form
+    {
+        private readonly string db;
+        private readonly InserimentoContatto c;
+        private OperazionePraticheEdili op;
+
+        public Ruoli(string dbNAme, InserimentoContatto c)
+        {
+            this.c = c;
+            db = dbNAme;
+            InitializeComponent();
+        }
+
+        private void Form6_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                op = new OperazionePraticheEdili(db);
+                var lista = op.CercaRuolo();
+                if (lista != null)
+                {
+                    dataGridView1.DataSource = lista;
+                    dataGridView1.Columns[0].Visible = false;
+                }
+                else
+                {
+                    var frm = new InserimentoRuolo(db, c);
+                    frm.Show();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Impossibile accedere a quest'area !!!");
+                Application.Exit();
+            }
+        }
+
+        private void DataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            foreach (DataGridViewCell cella in dataGridView1.Rows[e.RowIndex].Cells) cella.Style.ForeColor = Color.Red;
+        }
+
+        private void AggiornaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow riga in dataGridView1.Rows)
+                if (riga.Cells[0].Value != null)
+                    if (riga.Cells[0].Style.ForeColor == Color.Red)
+                        try
+                        {
+                            op.UpdateRuolo((int) riga.Cells["ID"].Value, riga.Cells["NOME"].Value + "",
+                                riga.Cells["DESCRIZIONE"].Value + "");
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Errore nell'inserimento di dati controllare l'inserimento", "Errore",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+
+            dataGridView1.DataSource = op.CercaRuolo();
+            dataGridView1.Columns[0].Visible = false;
+        }
+
+        private void AggiungiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var frm = new InserimentoRuolo(db, c);
+            frm.Show();
+        }
+
+        private void EliminaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows != null)
+                if (MessageBox.Show(
+                        "Stai per eliminare " +
+                        (string) dataGridView1.Rows[dataGridView1.SelectedRows[0].Index].Cells[1].Value + " .Confermi?",
+                        "Conferma Eliminazione richiesta:", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning) ==
+                    DialogResult.Yes)
+                    try
+                    {
+                        var clienti = op.CercaRuoloId((int) dataGridView1.Rows[dataGridView1.SelectedRows[0].Index]
+                            .Cells[0].Value);
+                        op.CancellaRuolo((int) dataGridView1.Rows[dataGridView1.SelectedRows[0].Index].Cells[0].Value);
+                        MessageBox.Show("Ruolo Eliminato", "Conferma", MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Impossibile cancellare la riga selezionata", "Errore:", MessageBoxButtons.OK,
+                            MessageBoxIcon.Error);
+                    }
+
+            dataGridView1.DataSource = op.CercaRuolo();
+            dataGridView1.Columns[0].Visible = false;
+        }
+    }
+}
