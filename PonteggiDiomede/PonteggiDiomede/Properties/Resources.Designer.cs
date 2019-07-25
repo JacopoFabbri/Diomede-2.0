f8489fe334e25a1ee19de2bb9acebd95776c8fc3
Andrea Passarella <duddyp@hotmail.com>
2019-07-25 16:23:20 +0200

    hnvvbn

--- PonteggiDiomede/PonteggiDiomede/Properties/Resources.Designer.cs
+++ PonteggiDiomede/PonteggiDiomede/Properties/Resources.Designer.cs
@@ -63,9 +63,119 @@ namespace PonteggiDiomede.Properties {
         /// <summary>
         ///   Cerca una risorsa localizzata di tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap icons8_apri_libro_64 {
+            get {
+                object obj = ResourceManager.GetObject("icons8-apri-libro-64", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Cerca una risorsa localizzata di tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_azienda_64 {
+            get {
+                object obj = ResourceManager.GetObject("icons8-azienda-64", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Cerca una risorsa localizzata di tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap icons8_eliminare_48 {
             get {
                 object obj = ResourceManager.GetObject("icons8-eliminare-48", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Cerca una risorsa localizzata di tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_inserisci_tabella_48 {
+            get {
+                object obj = ResourceManager.GetObject("icons8-inserisci-tabella-48", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Cerca una risorsa localizzata di tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_lista_di_cose_da_fare_100 {
+            get {
+                object obj = ResourceManager.GetObject("icons8-lista-di-cose-da-fare-100", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Cerca una risorsa localizzata di tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_modifica_file_48 {
+            get {
+                object obj = ResourceManager.GetObject("icons8-modifica-file-48", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Cerca una risorsa localizzata di tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_modifica_utente_uomo_16 {
+            get {
+                object obj = ResourceManager.GetObject("icons8-modifica-utente-uomo-16", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Cerca una risorsa localizzata di tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_nessuna_modifica_48 {
+            get {
+                object obj = ResourceManager.GetObject("icons8-nessuna-modifica-48", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Cerca una risorsa localizzata di tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_quaderno_48 {
+            get {
+                object obj = ResourceManager.GetObject("icons8-quaderno-48", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Cerca una risorsa localizzata di tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_rimuovi_immagine_48 {
+            get {
+                object obj = ResourceManager.GetObject("icons8-rimuovi-immagine-48", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Cerca una risorsa localizzata di tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_società_cliente_48 {
+            get {
+                object obj = ResourceManager.GetObject("icons8-società-cliente-48", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Cerca una risorsa localizzata di tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_web_48 {
+            get {
+                object obj = ResourceManager.GetObject("icons8-web-48", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
