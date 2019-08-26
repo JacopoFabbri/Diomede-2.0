d145e9accbfd1977a5ac74ded7e7a9357295829c
Jacopo Fabbri <jacopo.fabbri.work@gmail.com>
2019-08-26 12:14:14 +0200

    gadfhd

---
+++ Amministrazione/Amministrazione/Pratiche Edili/InserimentoCliente.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Diomede4
+{
+    public partial class InserimentoCliente : Form
+    {
+        private readonly string db;
+
+
+        public InserimentoCliente(string dbName)
+        {
+            db = dbName;
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var op = new OperazionePraticheEdili(db);
+                op.InserimentoCliente(textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text, textBox5.Text,
+                    textBox6.Text, textBox7.Text, textBox8.Text, textBox9.Text);
+                MessageBox.Show("Cliente Inserito:", "Inserito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Errore durante l'inserimento \nripetere l'operazione.","Errore:",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+
+        private void InserimentoCliente_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
